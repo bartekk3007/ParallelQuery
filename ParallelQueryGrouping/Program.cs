bc9e429cd9d6138c3b56731c10d5e8c285703532
bartekk3007 <149260538+bartekk3007@users.noreply.github.com>
2024-06-15 23:36:17 +0200

    Add files via upload

---
+++ ParallelQueryGrouping/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace ParallelQueryGrouping
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] data = new string[] 
+            {
+                "ananas", "azbest", "antena", "akordeon", "abecadlo",
+                "banan", "beczka", "bit", "bajt",
+                "cebula", "czerwien", "czern",
+                "dab", "derby",
+                "ekran"
+            };
+
+            ParallelQuery<IGrouping<char, string>> parallelGroups;
+            parallelGroups =
+                from text in data.AsParallel()
+                orderby text
+                group text by text[0];
+
+            foreach (var group in parallelGroups)
+            {
+                foreach(var item in group)
+                {
+                    Console.Write(item + ' ');
+                }
+                Console.WriteLine();
+            }
+
+            IDictionary<int, string> map = new Dictionary<int, string>();
+        }
+    }
+}
